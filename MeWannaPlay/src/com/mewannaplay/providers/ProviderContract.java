45a156a086ed8c0f093f0af827bc611d3df020de
vishalchangrani <vishalchangrani@localhost>
2011-11-14 23:27:44 -0800

    refactoring and providing ProviderContract

---
+++ MeWannaPlay/src/com/mewannaplay/providers/ProviderContract.java
@@ -0,0 +1,47 @@
+package com.mewannaplay.providers;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
+/**
+ * A little helper class that keeps all of the columns organized and readily accessible
+ * @author vishal
+ *
+ */
+public final class ProviderContract
+{
+	private ProviderContract() {
+	}
+	
+	public static final String AUTHORITY = "com.mewannaplay.providers.TennisCourtProvider";
+	public static final Uri AUTHORITY_URI = Uri.parse("content://"+AUTHORITY);
+	
+	//One BaseColumn implementation for each table we will be storing 
+	//Each implementation has content_uri to access the table, content_types and table columns and more.
+	public final static class TennisCourts implements BaseColumns {
+	
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(
+		            AUTHORITY_URI, DatabaseHelper.TENNIS_COURT_TABLE_NAME);
+		 
+		public static final String TENNIS_COURTS_CONTENT_TYPE = "vnd.android.cursor.dir/vnd.mewannaplay.tenniscourts";
+
+		public static final String TENNIS_COURT_ID = "_id";
+
+		public static final String TENNIS_LATITUDE = "tennis_latitude";
+
+		public static final String TENNIS_LONGITUDE = "tennis_longitude";
+		
+		public static final String TENNIS_SUBCOURTS = "tennis_subcourts";
+
+		public static final String OCCUPIED = "occupied";
+		
+		public static final String TENNIS_FACILITY_TYPE = "tennis_facility_type";
+
+		public static final String TENNIS_NAME = "tennis_name";
+		
+		public static final String MESSAGE_COUNT = "message_count";
+
+	}
+}
+
