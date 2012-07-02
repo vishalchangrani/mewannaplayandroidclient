fc10469632891c0c9963d25bb71a7f2606698ca1
Vishal <vishalchangrani@users.noreply.github.com>
2012-07-03 03:32:30 +0600

    Fix INFO and courtdetails UI

--- MeWannaPlayAndroidClient/src/com/mewannaplay/Info.java
+++ MeWannaPlayAndroidClient/src/com/mewannaplay/Info.java
@@ -2,13 +2,25 @@ package com.mewannaplay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Info extends Activity{
+public class Info extends Activity implements OnClickListener{
+	Button ok;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.info);
+		ok=(Button)findViewById(R.id.btnok);
+		ok.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		finish();
+		
 	}
 
 }
