2532dc63fe556ff5f5c55a4c761b7db6d04fb2f7
Vishal <vishalchangrani@users.noreply.github.com>
2012-07-12 23:32:36 -0400

    preventing removal of mylocationoverlay in onpause and chaning syncadapter to avoid some interrupted exceptions

---
+++ MeWannaPlay/src/com/mewannaplay/model/Status.java
@@ -0,0 +1,38 @@
+package com.mewannaplay.model;
+
+public class Status {
+	private String error;
+	private int code;
+	private String description;
+	private String message;
+	
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public final boolean isNotSuccess()
+	{
+		return code != 200;
+	}
+}
