bb2a0fe8f3e007e2802d5c8215573877fd054936
powerandroid <powerandroid@localhost>
2012-07-25 23:41:10 +0600

    Fixed ui

--- MeWannaPlayAndroidClient/src/com/mewannaplay/asynctask/GetCourtDetailsAsyncTask.java
+++ MeWannaPlayAndroidClient/src/com/mewannaplay/asynctask/GetCourtDetailsAsyncTask.java
@@ -18,47 +18,47 @@ import com.mewannaplay.providers.ProviderContract;
 
 public class GetCourtDetailsAsyncTask extends BasicAsyncTask {
 
-	private String TAG = "GetCourtDetailsAsyncTask";
-	private final int courtId;
-	private final Location location;
-	
-	public GetCourtDetailsAsyncTask(Activity context ,int courtId, Location location) {
-		super(context ,Constants.GET_TENNISCOURT_DETAILS + courtId, RequestMethods.GET, "Retreiving court details...");
-		this.courtId = courtId;
-		this.location = location;
-	}
+        private String TAG = "GetCourtDetailsAsyncTask";
+        private final int courtId;
+        private final Location location;
+        
+        public GetCourtDetailsAsyncTask(Activity context ,int courtId, Location location) {
+                super(context ,Constants.GET_TENNISCOURT_DETAILS + courtId, RequestMethods.GET, "Retreiving court details...");
+                this.courtId = courtId;
+                this.location = location;
+        }
 
 
-	@Override
-	protected void parseResponse(JSONObject jsonObject) throws IOException {
-		try {
-			TennisCourtDetails tdc = TennisCourtDetails
-					.fromJSONObject(jsonObject);
-			ownerAcitivty
-					.getContentResolver()
-					.insert(ProviderContract.TennisCourtsDetails.CONTENT_URI
-							.buildUpon().appendPath(courtId + "").build(),
-							tdc.toContentValue());
-			ownerAcitivty
-			.getContentResolver()
-			.bulkInsert(ProviderContract.Acitivity.CONTENT_URI,
-					tdc.contentValuesForActivity());
-			ownerAcitivty
-			.getContentResolver()
-			.bulkInsert(ProviderContract.Amenity.CONTENT_URI,
-					tdc.contentValuesForAmenity());
-		} catch (Exception e) {
-			Log.e(TAG, " getCourtDetails "+e.getMessage());
-			throw new IOException(" Conversion error ");
-		}
-		
-	}
-	
-	 
-	@Override
-	protected void onPostExecute(Boolean result) {
-		super.onPostExecute(result);
-		((MapViewActivity) ownerAcitivty).onPostExecuteGetCourtDetails(result, courtId, location);
-	}
-	
+        @Override
+        protected void parseResponse(JSONObject jsonObject) throws IOException {
+                try {
+                        TennisCourtDetails tdc = TennisCourtDetails
+                                        .fromJSONObject(jsonObject);
+                        ownerAcitivty
+                                        .getContentResolver()
+                                        .insert(ProviderContract.TennisCourtsDetails.CONTENT_URI
+                                                        .buildUpon().appendPath(courtId + "").build(),
+                                                        tdc.toContentValue());
+                        ownerAcitivty
+                        .getContentResolver()
+                        .bulkInsert(ProviderContract.Acitivity.CONTENT_URI,
+                                        tdc.contentValuesForActivity());
+                        ownerAcitivty
+                        .getContentResolver()
+                        .bulkInsert(ProviderContract.Amenity.CONTENT_URI,
+                                        tdc.contentValuesForAmenity());
+                } catch (Exception e) {
+                        Log.e(TAG, " getCourtDetails "+e.getMessage());
+                        throw new IOException(" Conversion error ");
+                }
+                
+        }
+        
+         
+        @Override
+        protected void onPostExecute(Boolean result) {
+                super.onPostExecute(result);
+                ((MapViewActivity) ownerAcitivty).onPostExecuteGetCourtDetails(result, courtId, location);
+        }
+        
 }
--- MeWannaPlayAndroidClient/src/com/mewannaplay/asynctask/GetPostedMessageAsyncTask.java
+++ MeWannaPlayAndroidClient/src/com/mewannaplay/asynctask/GetPostedMessageAsyncTask.java
@@ -16,43 +16,43 @@ import com.mewannaplay.providers.ProviderContract;
 
 public class GetPostedMessageAsyncTask extends BasicAsyncTask {
 
-	public GetPostedMessageAsyncTask(Activity context, int courtId) {
-		super(context, Constants.GET_TENNISCOURT_MESSAGE_BY_COURTID + courtId, RequestMethods.GET,
-				"Retreiving posted message...");
-		
-	}
-	
-	 @Override
-	 protected Boolean doInBackground(Void... v) {
-		ownerAcitivty.getContentResolver().delete(ProviderContract.Messages.CONTENT_URI,
-					null, null); // clean message table
-		 return super.doInBackground(v);
-		
-	}
+        public GetPostedMessageAsyncTask(Activity context, int courtId) {
+                super(context, Constants.GET_TENNISCOURT_MESSAGE_BY_COURTID + courtId, RequestMethods.GET,
+                                "Retreiving posted message...");
+                
+        }
+        
+         @Override
+         protected Boolean doInBackground(Void... v) {
+                ownerAcitivty.getContentResolver().delete(ProviderContract.Messages.CONTENT_URI,
+                                        null, null); // clean message table
+                 return super.doInBackground(v);
+                
+        }
 
-	@Override
-	protected void parseResponse(JSONObject jsonObject) throws IOException {
-	   	Log.d(TAG, " in getCourtMessageByTennisCourtId...");
+        @Override
+        protected void parseResponse(JSONObject jsonObject) throws IOException {
+                Log.d(TAG, " in getCourtMessageByTennisCourtId...");
    
-    	ContentValues contentValue = new ContentValues();
-    	try {
-			Message message = Message.fromSingleJSONObject(jsonObject);
-			contentValue = message.toContentValue();
-    	}
-    	 catch (Exception e) {
- 			Log.e(TAG, e.getMessage());
- 			throw new IOException(" Conversion error ");
- 		}
-			Log.d(TAG, "now calling bulk insert for this message");
-			ownerAcitivty
-					.getContentResolver()
-					.bulkInsert(ProviderContract.Messages.CONTENT_URI,
-							new ContentValues[] { contentValue});
-	}
+        ContentValues contentValue = new ContentValues();
+        try {
+                        Message message = Message.fromSingleJSONObject(jsonObject);
+                        contentValue = message.toContentValue();
+        }
+         catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                        throw new IOException(" Conversion error ");
+                }
+                        Log.d(TAG, "now calling bulk insert for this message");
+                        ownerAcitivty
+                                        .getContentResolver()
+                                        .bulkInsert(ProviderContract.Messages.CONTENT_URI,
+                                                        new ContentValues[] { contentValue});
+        }
 
-	@Override
-	protected void onPostExecute(Boolean result) {
-		super.onPostExecute(result);
-		((MapViewActivity) ownerAcitivty).onPostExecutePostedMessageTask(result);
-	}
+        @Override
+        protected void onPostExecute(Boolean result) {
+                super.onPostExecute(result);
+                ((MapViewActivity) ownerAcitivty).onPostExecutePostedMessageTask(result);
+        }
 }
