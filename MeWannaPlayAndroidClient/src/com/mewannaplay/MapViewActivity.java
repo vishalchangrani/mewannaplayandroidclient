e977b2fa6809e97f93eb7270ed63c2c8bfcacde7
Vishal <vishalchangrani@users.noreply.github.com>
2012-07-29 21:27:53 -0400

    fixing bug of showing both 'no messages" and the actual message list.

--- MeWannaPlayAndroidClient/src/com/mewannaplay/MapViewActivity.java
+++ MeWannaPlayAndroidClient/src/com/mewannaplay/MapViewActivity.java
@@ -240,9 +240,7 @@ public class MapViewActivity extends MapActivity implements OnClickListener {
                 Log.d(TAG, " Adding continous refresh for court statistics");
                 ContentResolver.setSyncAutomatically(MapViewActivity.getAccount(this),
                                 ProviderContract.AUTHORITY, true);
-                boolean value = ContentResolver.getMasterSyncAutomatically();
 
-                
                 ContentResolver.setIsSyncable(MapViewActivity.getAccount(this), ProviderContract.AUTHORITY, 1);
 
                 // Periodically update courts occupied count and message count for all
